f6dfdf0a2c8106025b3ef8d0fa7f4ba922e909fa
hattori-hanz0 <hattori@tuta.io>
2021-11-28 18:55:31 +0100

    2021-11-28-18:55:31

--- install.sh
+++ install.sh
@@ -13,9 +13,14 @@ mkdir -p $HOME/bin
 mkdir -p $HOME/Notes
 mkdir -p $HOME/.config/nvim
 
-git clone https://github.com/hattori-hanz0/neovim-lua $NEOVIM_LUA_DIR
+git clone https://github.com/hattori-hanz0/neovim-lua \
+    $NEOVIM_LUA_DIR
 
-git clone https://github.com/sainnhe/everforest $HOME/.local/share/nvim/site/pack/packer/start
+git clone https://github.com/sainnhe/everforest \
+    $HOME/.local/share/nvim/site/pack/packer/start/everforest
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 ln -sf $NEOVIM_LUA_DIR/.config/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf $NEOVIM_LUA_DIR/.config/vars ~/.config/vars
@@ -24,9 +29,6 @@ ln -sf $NEOVIM_LUA_DIR/docs ~/.config/nvim/docs
 
 ln -sf $NEOVIM_LUA_DIR/bin/gp.sh ~/bin/gp.sh
 ln -sf $NEOVIM_LUA_DIR/bin/ga.sh ~/bin/ga.sh
-
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 

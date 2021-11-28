b80b64a521cba6bc5ef638755a8f2b59a5fa1fcc
hattori-hanz0 <hattori@tuta.io>
2021-11-29 00:08:28 +0100

    2021-11-29-00:08:28

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1102,7 +1102,7 @@ ins_left({
   function()
     local current_line = vim.fn.line(".")
     local total_lines = vim.fn.line("$")
-    local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+    local chars = { "_", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
     local line_ratio = current_line / total_lines
     local index = math.ceil(line_ratio * #chars)
     return chars[index]

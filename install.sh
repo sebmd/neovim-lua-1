6417ef85109c645d241a6e46431893426a80b988
hattori-hanz0 <hattori@tuta.io>
2021-11-28 16:40:36 +0100

    2021-11-28-16:40:36

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ SKRYPT="$PWD/$(basename $0)"
 PLIK=${SKRYPT##*/}
 # katalog skryptu
 KATALOG=${SKRYPT%$PLIK}
+
+git clone https://github.com/hattori-hanz0/neovim-lua ~/.local/share/
+
+ln -sf ~/.local/share/neovim-lua/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/.local/share/neovim-lua/.config/vars ~/.config/vars
+ln -sf ~/.local/share/neovim-lua/README.md ~/.config/nvim/README.md
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+

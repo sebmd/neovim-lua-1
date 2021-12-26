41817a36ddcdfb83db04db7069c0d07e1ed53cc2
hattori-hanz0 <hattori@tuta.io>
2021-12-26 16:34:17 +0100

    2021-12-26-16:34:17

--- .config/nvim/lua/plugins/pears.lua
+++ .config/nvim/lua/plugins/pears.lua
@@ -1,5 +1,10 @@
 -- pears
-require("pears").setup(function(conf)
+local pears_status_ok, configs = pcall(require, "pears")
+if not pears_status_ok then
+  return
+end
+
+configs.setup(function(conf)
   conf.pair("{", "}")
   conf.expand_on_enter(false)
 end)

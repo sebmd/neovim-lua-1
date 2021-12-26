217773e45fd0f951adab8dc499072050d43617a4
hattori-hanz0 <hattori@tuta.io>
2021-12-26 16:05:21 +0100

    2021-12-26-16:05:21

--- .config/nvim/lua/plugins/cheatsheet.lua
+++ .config/nvim/lua/plugins/cheatsheet.lua
@@ -1,5 +1,10 @@
 -- cheatsheet
-require("cheatsheet").setup({
+local cheatsheet_status_ok, configs = pcall(require, "cheatsheet")
+if not cheatsheet_status_ok then
+  return
+end
+
+configs.setup({
   bundled_cheatsheets = true,
   bundled_plugin_cheatsheets = true,
   include_only_installed_plugins = true,

ee83400e475c9aa5d0779727b9c498680fab916b
hattori-hanz0 <hattori@tuta.io>
2021-12-26 16:18:41 +0100

    2021-12-26-16:18:41

--- .config/nvim/lua/plugins/kommentary.lua
+++ .config/nvim/lua/plugins/kommentary.lua
@@ -1,4 +1,14 @@
 -- kommentary
+local kommentary_status_ok, configs = pcall(require, "kommentary.config")
+if not kommentary_status_ok then
+  return
+end
+
+configs.configure_language("rust", {
+  single_line_comment_string = "//",
+  multi_line_comment_strings = { "/*", "*/" },
+})
+
 g.kommentary_create_default_mappings = false
 
 -- api.nvim_set_keymap("n", "<leader>cic", "<Plug>kommentary_line_increase", {})
@@ -12,8 +22,3 @@ g.kommentary_create_default_mappings = false
 -- api.nvim_set_keymap("n", "<c-_>", "<Plug>kommentary_line_default", {})
 -- api.nvim_set_keymap("n", "<leader>c", "<Plug>kommentary_motion_default", {})
 -- api.nvim_set_keymap("x", "<c-_>", "<Plug>kommentary_visual_default", {})
-
-require("kommentary.config").configure_language("rust", {
-  single_line_comment_string = "//",
-  multi_line_comment_strings = { "/*", "*/" },
-})

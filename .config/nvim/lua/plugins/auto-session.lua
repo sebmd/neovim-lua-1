1643bf3eabbe68cfcf44cea3d7f4f1b6b9f09a89
hattori-hanz0 <hattori@tuta.io>
2022-01-12 03:42:57 +0100

    2022-01-12-03:42:57

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,13 +1,19 @@
 -- auto-session
--- local sessionopts = {
 -- SaveSession
 -- RestoreSession
--- log_level = "info",
--- auto_session_enable_last_session = false,
--- auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
--- auto_session_enabled = true,
--- auto_save_enabled = true,
--- auto_restore_enabled = false,
--- auto_session_suppress_dirs = nil,
--- }
--- require("auto-session").setup(sessionopts)
+local as_status_ok, configs = pcall(require, "auto-session")
+if not as_status_ok then
+  return
+end
+
+local sessionopts = {
+  log_level = "info",
+  auto_session_enable_last_session = false,
+  auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+  auto_session_enabled = true,
+  auto_save_enabled = true,
+  auto_restore_enabled = false,
+  auto_session_suppress_dirs = nil,
+}
+
+config.setup(sessionopts)
